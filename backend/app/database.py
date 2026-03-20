93c48cd8a425679da3728314695d7967e00923dd
cyb3rh0lic <zeha8917@gmail.com>
2026-03-20 20:17:59 +0900

    chore: database.py 파일 수정

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,13 +3,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
+from urllib.parse import quote_plus
+
 
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST", "192.168.80.60")
 DB_PORT = os.getenv("DB_PORT", "3306")
 DB_USER = os.getenv("DB_USER", "pms_user")
-DB_PASSWORD = os.getenv("pms_pass_123")
+DB_PASSWORD = quote_plus(os.getenv("password", ""))
 DB_NAME = os.getenv("DB_NAME", "hotel_pms")
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
@@ -24,3 +26,4 @@ def get_db():
         yield db
     finally:
         db.close()
+
